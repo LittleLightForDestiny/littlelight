700cbe289c5075660184fa60ad76c4e34d034691
Joao Paulo Marquesini <jao@markezine.me>
2019-07-02 17:00:55 -0300

    initial desktop version

---
+++ desktop_plugins/path_provider_fde/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,11 @@
+//
+//  Generated file. Do not edit.
+//
+import Foundation
+import FlutterMacOS
+
+import path_provider_fde
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
