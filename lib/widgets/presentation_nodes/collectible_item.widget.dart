191a665067ae7b7e474de7299d4ce8138e0d0e53
João Paulo Marquesini <jmarquesini@gmail.com>
2020-03-02 13:38:08 -0300

    fixing multiselect and adding more perks on duplicates view

--- lib/widgets/presentation_nodes/collectible_item.widget.dart
+++ lib/widgets/presentation_nodes/collectible_item.widget.dart
@@ -10,6 +10,7 @@ import 'package:little_light/services/bungie_api/bungie_api.service.dart';
 import 'package:little_light/services/manifest/manifest.service.dart';
 import 'package:little_light/services/profile/profile.service.dart';
 import 'package:little_light/services/selection/selection.service.dart';
+import 'package:little_light/services/user_settings/user_settings.service.dart';
 import 'package:little_light/utils/item_with_owner.dart';
 import 'package:little_light/widgets/common/queued_network_image.widget.dart';
 import 'package:little_light/widgets/item_list/items/armor/armor_inventory_item.widget.dart';
@@ -194,6 +195,7 @@ class CollectibleItemWidgetState extends State<CollectibleItemWidget> {
       onLongPress(context);
       return;
     }
+    
     DestinyInventoryItemDefinition itemDef = await widget.manifest
         .getDefinition<DestinyInventoryItemDefinition>(definition.itemHash);
     if (itemDef == null) {
@@ -202,8 +204,7 @@ class CollectibleItemWidgetState extends State<CollectibleItemWidget> {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) =>
-            ItemDetailScreen(definition:itemDef),
+        builder: (context) => ItemDetailScreen(definition: itemDef),
       ),
     );
   }
