a4ed73e3089cbb8a7765b51b5806b849d43d7404
João Paulo Marquesini <jmarquesini@gmail.com>
2020-06-17 18:09:41 -0300

    v1.7.08

--- macos/Pods/Target Support Files/Pods-Runner/Pods-Runner-frameworks.sh
+++ macos/Pods/Target Support Files/Pods-Runner/Pods-Runner-frameworks.sh
@@ -162,7 +162,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
-  install_framework "${PODS_ROOT}/../Flutter/ephemeral/.symlinks/flutter/darwin-x64/FlutterMacOS.framework"
+  install_framework "${PODS_ROOT}/../Flutter/ephemeral/.symlinks/flutter/darwin-x64-release/FlutterMacOS.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/package_info_fde/package_info_fde.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/path_provider_fde/path_provider_fde.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/screen_fde/screen_fde.framework"
@@ -173,7 +173,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
-  install_framework "${PODS_ROOT}/../Flutter/ephemeral/.symlinks/flutter/darwin-x64/FlutterMacOS.framework"
+  install_framework "${PODS_ROOT}/../Flutter/ephemeral/.symlinks/flutter/darwin-x64-release/FlutterMacOS.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/package_info_fde/package_info_fde.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/path_provider_fde/path_provider_fde.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/screen_fde/screen_fde.framework"
@@ -184,7 +184,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Profile" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
-  install_framework "${PODS_ROOT}/../Flutter/ephemeral/.symlinks/flutter/darwin-x64/FlutterMacOS.framework"
+  install_framework "${PODS_ROOT}/../Flutter/ephemeral/.symlinks/flutter/darwin-x64-release/FlutterMacOS.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/package_info_fde/package_info_fde.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/path_provider_fde/path_provider_fde.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/screen_fde/screen_fde.framework"
