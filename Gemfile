3b16944e27aba0ddae3e3fd98f23fd08ad60dced
João Paulo Marquesini <jao@markezine.me>
2021-03-09 00:42:33 -0300

    moving fastlane to project root

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+gem "dotenv"
+gem "fastlane"
+
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/littlelight/sdks/flutter-stable"
-export "FLUTTER_APPLICATION_PATH=/Users/littlelight/workspace/littlelight_desktop"
+export "FLUTTER_APPLICATION_PATH=/Users/littlelight/workspace/littlelight"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_BUILD_NAME=1.7.51"
-export "FLUTTER_BUILD_NUMBER=107051"
+export "FLUTTER_BUILD_NAME=1.7.53"
+export "FLUTTER_BUILD_NUMBER=107053"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=true"
-export "PACKAGE_CONFIG=/Users/littlelight/workspace/littlelight_desktop/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/littlelight/workspace/littlelight/.dart_tool/package_config.json"
