76f6b929ec65f4ec1416084a3e73eeb889b6a55c
João Paulo Marquesini <joao.marquesini@estrategiaconcursos.com.br>
2022-02-12 03:17:39 -0300

    change format to use dart instead of flutter

--- lib/models/item_notes_response.dart
+++ lib/models/item_notes_response.dart
@@ -5,6 +5,8 @@ import 'package:json_annotation/json_annotation.dart';
 import 'item_notes.dart';
 import 'item_notes_tag.dart';
 
+part 'item_notes_response.g.dart';
+
 @JsonSerializable()
 class NotesResponse {
   List<ItemNotes> notes;
---
+++ lib/models/item_notes_response.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// @dart=2.12
+
+part of 'item_notes_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+NotesResponse _$NotesResponseFromJson(Map<String, dynamic> json) =>
+    NotesResponse(
+      notes: (json['notes'] as List<dynamic>?)
+          ?.map((e) => ItemNotes.fromJson(e))
+          .toList(),
+      tags: (json['tags'] as List<dynamic>?)
+          ?.map((e) => ItemNotesTag.fromJson(e))
+          .toList(),
+    );
+
+Map<String, dynamic> _$NotesResponseToJson(NotesResponse instance) =>
+    <String, dynamic>{
+      'notes': instance.notes,
+      'tags': instance.tags,
+    };
