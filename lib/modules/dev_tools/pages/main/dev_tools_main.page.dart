b81b85c8a041a1b6a52f9619ca43a040f0b13bd2
bill0042 <68356139+bill0042@users.noreply.github.com>
2023-04-18 23:18:12 -0700

    Testing full and partial scan of manifest

--- lib/modules/dev_tools/pages/main/dev_tools_main.page.dart
+++ lib/modules/dev_tools/pages/main/dev_tools_main.page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:little_light/modules/dev_tools/pages/stats/dev_tools_stats.page_route.dart';
+import 'package:little_light/services/manifest/manifest.consumer.dart';
 
-class DevToolsPage extends StatelessWidget {
+class DevToolsPage extends StatelessWidget with ManifestConsumer {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +23,36 @@ class DevToolsPage extends StatelessWidget {
               onPressed: () => Navigator.of(context).push(DevToolsStatsPageRoute()),
               style: ButtonStyle(visualDensity: VisualDensity.standard),
               child: Text("Stats"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                manifest.itemScanTest(1);
+              },
+              style: ButtonStyle(visualDensity: VisualDensity.standard),
+              child: Text("Test 1:  Scan all rows"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                manifest.itemScanTest(2);
+              },
+              style: ButtonStyle(visualDensity: VisualDensity.standard),
+              child: Text("Test 2: Query fragments"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                manifest.itemScanTest(3);
+              },
+              style: ButtonStyle(visualDensity: VisualDensity.standard),
+              child: Text("Test 3: Query fragments and aspects"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                List<int> hashes =
+                    await manifest.getHashesByPatternSearch('DestinySocketTypeDefinition', '%masterworks.trackers%');
+                print("Got tracker hashes: ${hashes}");
+              },
+              style: ButtonStyle(visualDensity: VisualDensity.standard),
+              child: Text("Find tracker socketType hashes"),
             ),
           ],
         ),
