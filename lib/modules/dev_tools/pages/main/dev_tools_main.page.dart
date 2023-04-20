1bff382fe30d46ac236d9802f42385ff0df23c0c
bill0042 <68356139+bill0042@users.noreply.github.com>
2023-04-19 21:31:46 -0700

    Moved scan tests from manifest.service to new dev tools files

--- lib/modules/dev_tools/pages/main/dev_tools_main.page.dart
+++ lib/modules/dev_tools/pages/main/dev_tools_main.page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:little_light/modules/dev_tools/pages/stats/dev_tools_stats.page_route.dart';
-import 'package:little_light/services/manifest/manifest.consumer.dart';
+import 'package:little_light/modules/dev_tools/pages/manifest/dev_tools_manifest.page_route.dart';
 
-class DevToolsPage extends StatelessWidget with ManifestConsumer {
+class DevToolsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,34 +25,9 @@ class DevToolsPage extends StatelessWidget with ManifestConsumer {
               child: Text("Stats"),
             ),
             ElevatedButton(
-              onPressed: () async {
-                manifest.itemScanTest(1);
-              },
+              onPressed: () => Navigator.of(context).push(DevToolsManifestPageRoute()),
               style: ButtonStyle(visualDensity: VisualDensity.standard),
-              child: Text("Test 1:  Scan all rows"),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                manifest.itemScanTest(2);
-              },
-              style: ButtonStyle(visualDensity: VisualDensity.standard),
-              child: Text("Test 2: Query fragments"),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                manifest.itemScanTest(3);
-              },
-              style: ButtonStyle(visualDensity: VisualDensity.standard),
-              child: Text("Test 3: Query fragments and aspects"),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                List<int> hashes =
-                    await manifest.getHashesByPatternSearch('DestinySocketTypeDefinition', '%masterworks.trackers%');
-                print("Got tracker hashes: ${hashes}");
-              },
-              style: ButtonStyle(visualDensity: VisualDensity.standard),
-              child: Text("Find tracker socketType hashes"),
+              child: Text("Manifest"),
             ),
           ],
         ),
---
+++ lib/modules/dev_tools/pages/manifest/dev_tools_manifest.page_route.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'dev_tools_manifest.page.dart';
+
+class DevToolsManifestPageRoute extends MaterialPageRoute {
+  DevToolsManifestPageRoute() : super(builder: (context) => DevToolsManifestPage());
+}
