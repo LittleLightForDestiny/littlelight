a65db370a76d8031a8659fe7e38bc6ea78ad96b3
bill0042 <68356139+bill0042@users.noreply.github.com>
2023-05-13 00:03:53 -0700

    Added mail icon to show duplicate items in postmaster

--- lib/shared/widgets/inventory_item/duplicated_item.widget.dart
+++ lib/shared/widgets/inventory_item/duplicated_item.widget.dart
@@ -213,12 +213,23 @@ class DuplicatedItemWidget extends StatelessWidget {
     final profile = context.watch<ProfileBloc>();
     final character = profile.getCharacterById(item.characterId);
     if (character != null) {
-      return ManifestText<DestinyClassDefinition>(
+      final characterName = ManifestText<DestinyClassDefinition>(
         character.character.classHash,
         uppercase: true,
         style: context.textTheme.highlight,
         textExtractor: (def) => def.genderedClassNamesByGenderHash?["${character.character.genderHash}"],
       );
+      if (item.bucketHash == InventoryBucket.lostItems) {
+        return Row(children: [
+          characterName,
+          Container(
+            child: Icon(Icons.mail, size: characterName.style?.fontSize ?? 12),
+            //padding: EdgeInsets.only(left: 2),
+            padding: EdgeInsets.zero,
+          )
+        ]);
+      }
+      return characterName;
     }
     if (item.bucketHash == InventoryBucket.general) {
       return Text(
