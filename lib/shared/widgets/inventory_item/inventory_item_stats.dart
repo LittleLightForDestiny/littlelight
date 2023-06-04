54329819150e46476394333f5a57c5268454f22b
bill0042 <68356139+bill0042@users.noreply.github.com>
2023-06-03 22:25:29 -0700

    Fixed SmallArmorStatsWidget overflow, rearranged currencies with commas

--- lib/shared/widgets/inventory_item/inventory_item_stats.dart
+++ lib/shared/widgets/inventory_item/inventory_item_stats.dart
@@ -6,13 +6,9 @@ import 'package:little_light/shared/widgets/stats/small_armor_stats.widget.dart'
 class InventoryItemStats extends StatelessWidget with WishlistsConsumer {
   final DestinyItemInfo itemInfo;
   final double iconSize;
-  final double textWidth;
-  final EdgeInsets iconMargin;
   InventoryItemStats(
     this.itemInfo, {
     this.iconSize = 16,
-    this.textWidth = 18,
-    this.iconMargin = const EdgeInsets.only(right: 1),
   });
   @override
   Widget build(BuildContext context) {
@@ -20,8 +16,6 @@ class InventoryItemStats extends StatelessWidget with WishlistsConsumer {
     return SmallArmorStatsWidget(
       stats,
       iconSize: this.iconSize,
-      iconMargin: this.iconMargin,
-      textWidth: textWidth,
     );
   }
 }
