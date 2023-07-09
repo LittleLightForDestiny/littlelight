5e89106cb8dd1dd0f45db9824d1e1c7cf72f8020
João Paulo Marquesini <joao.marquesini@estrategia.com>
2023-07-09 03:22:55 -0300

    fixes interactions on vendor's duplicates'

--- lib/modules/item_details/pages/vendor_item_details/vendor_item_details.page.dart
+++ lib/modules/item_details/pages/vendor_item_details/vendor_item_details.page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:little_light/core/blocs/selection/selection.bloc.dart';
 import 'package:little_light/core/blocs/vendors/vendor_item_info.dart';
+import 'package:little_light/models/item_info/inventory_item_info.dart';
 import 'package:little_light/modules/item_details/blocs/item_details.bloc.dart';
 import 'package:little_light/modules/item_details/pages/vendor_item_details/vendor_item_details.bloc.dart';
 import 'package:little_light/modules/item_details/pages/vendor_item_details/vendor_item_socket_controller.bloc.dart';
+import 'package:little_light/shared/blocs/item_interaction_handler/item_interaction_handler.bloc.dart';
 import 'package:little_light/shared/blocs/scoped_value_repository/scoped_value_repository.bloc.dart';
 import 'package:little_light/shared/blocs/socket_controller/socket_controller.bloc.dart';
 import 'package:provider/provider.dart';
@@ -21,6 +23,13 @@ class VendorItemDetailsPage extends StatelessWidget {
         ChangeNotifierProvider<ScopedValueRepositoryBloc>(create: (context) => ScopedValueRepositoryBloc()),
         ChangeNotifierProvider<SocketControllerBloc>(create: (context) => VendorItemSocketControllerBloc(context)),
         ChangeNotifierProvider<ItemDetailsBloc>(create: (context) => VendorItemDetailsBloc(context, item: item)),
+        Provider<ItemInteractionHandlerBloc>(create: (context) {
+          final bloc = context.read<ItemDetailsBloc>();
+          return ItemInteractionHandlerBloc(
+            onTap: (item) => item is InventoryItemInfo ? bloc.onDuplicateItemTap(item) : null,
+            onHold: (item) => item is InventoryItemInfo ? bloc.onDuplicateItemHold(item) : null,
+          );
+        }),
       ],
       builder: (context, _) => VendorItemDetailsView(
         context.read<ItemDetailsBloc>(),
