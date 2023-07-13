cef17e5deee1ae5ac55950e9bf5fed3c7d3a59ef
João Paulo Marquesini <joao.marquesini@estrategia.com>
2023-07-13 13:20:12 -0300

    added epic platform data

--- lib/utils/platform_data.dart
+++ lib/utils/platform_data.dart
@@ -1,5 +1,6 @@
 import 'package:bungie_api/enums/bungie_membership_type.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:little_light/widgets/icon_fonts/littlelight_icons.dart';
 
 extension MembershipPlatformData on BungieMembershipType {
@@ -18,32 +19,29 @@ class PlatformData {
   static PlatformData getPlatform(BungieMembershipType type) {
     switch (type) {
       case BungieMembershipType.TigerXbox:
-        return PlatformData("Xbox Live", LittleLightIcons.platform_xbox,
-            const Color.fromARGB(255, 93, 194, 30));
+        return PlatformData("Xbox Live", LittleLightIcons.platform_xbox, const Color.fromARGB(255, 93, 194, 30));
 
       case BungieMembershipType.TigerPsn:
         return PlatformData(
-            "Playstation Network",
-            LittleLightIcons.platform_playstation,
-            const Color.fromARGB(255, 0, 55, 145));
+            "Playstation Network", LittleLightIcons.platform_playstation, const Color.fromARGB(255, 0, 55, 145));
 
       case BungieMembershipType.TigerBlizzard:
-        return PlatformData("Battle.net", LittleLightIcons.platform_blizzard,
-            const Color.fromARGB(255, 0, 180, 255));
+        return PlatformData("Battle.net", LittleLightIcons.platform_blizzard, const Color.fromARGB(255, 0, 180, 255));
 
       case BungieMembershipType.TigerSteam:
-        return PlatformData("Steam", LittleLightIcons.platform_steam,
-            const Color.fromARGB(255, 42, 71, 94));
+        return PlatformData("Steam", LittleLightIcons.platform_steam, const Color.fromARGB(255, 42, 71, 94));
 
       case BungieMembershipType.TigerStadia:
-        return PlatformData("Stadia", LittleLightIcons.platform_stadia,
-            const Color.fromARGB(255, 205, 38, 64));
+        return PlatformData("Stadia", LittleLightIcons.platform_stadia, const Color.fromARGB(255, 205, 38, 64));
+
+      case BungieMembershipType.TigerEgs:
+        return PlatformData("Epic Games", FontAwesomeIcons.e, const Color.fromARGB(255, 93, 194, 30));
 
       default:
         return PlatformData("Unknown", Icons.not_interested, Colors.black);
     }
   }
 
-  static PlatformData get crossPlayData => PlatformData("Bungie cross save",
-      LittleLightIcons.cross_save, const Color.fromARGB(255, 0, 180, 255));
+  static PlatformData get crossPlayData =>
+      PlatformData("Bungie cross save", LittleLightIcons.cross_save, const Color.fromARGB(255, 0, 180, 255));
 }
