4f2779f58980fc95ae6919a0abfac064c768e289
João Paulo Marquesini <joao.marquesini@estrategia.com>
2023-07-16 16:13:35 -0300

    add higher resolution class overlays for 2/3x devices

--- lib/shared/widgets/character/base_character_icon.widget.dart
+++ lib/shared/widgets/character/base_character_icon.widget.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:little_light/core/theme/littlelight.theme.dart';
-import 'package:bungie_api/src/enums/destiny_class.dart';
 
 const characterIconDefaultBorderWidth = 1.5;
 
 abstract class BaseCharacterIconWidget extends StatelessWidget {
   final double borderWidth;
-  final bool hideClassIcon;
 
   const BaseCharacterIconWidget({
     Key? key,
     this.borderWidth = characterIconDefaultBorderWidth,
-    this.hideClassIcon = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final classType = getClassType(context);
+    final overlays = buildOverlays(context);
     return Stack(children: [
       Container(
         decoration: BoxDecoration(
@@ -29,28 +26,11 @@ abstract class BaseCharacterIconWidget extends StatelessWidget {
         ),
         child: buildIcon(context),
       ),
-      if (!hideClassIcon && classType == DestinyClass.Titan)
-        Positioned(
-          left: borderWidth * 2,
-          bottom: borderWidth * 1,
-          child: Container(child: Image.asset("assets/imgs/class_titan_bordered.png", scale: 1.0)),
-        ),
-      if (!hideClassIcon && classType == DestinyClass.Warlock)
-        Positioned(
-          left: borderWidth * 1,
-          bottom: borderWidth * 2,
-          child: Container(child: Image.asset("assets/imgs/class_warlock_bordered.png", scale: 1.0)),
-        ),
-      if (!hideClassIcon && classType == DestinyClass.Hunter)
-        Positioned(
-          left: borderWidth * 2,
-          bottom: borderWidth * 2,
-          child: Container(child: Image.asset("assets/imgs/class_hunter_bordered.png", scale: 1.0)),
-        )
+      if (overlays != null) ...overlays,
     ]);
   }
 
-  Widget buildIcon(BuildContext context);
+  List<Positioned>? buildOverlays(BuildContext context) => null;
 
-  DestinyClass? getClassType(BuildContext context) => null;
+  Widget buildIcon(BuildContext context);
 }
