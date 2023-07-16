5812e60faa46a466a01987868efdc9c0e2d6ea6d
João Paulo Marquesini <joao.marquesini@estrategia.com>
2023-07-16 20:59:39 -0300

    added gold background on enhanced perks

--- lib/shared/widgets/sockets/perk_icon.widget.dart
+++ lib/shared/widgets/sockets/perk_icon.widget.dart
@@ -48,6 +48,7 @@ class PerkIconWidget extends StatelessWidget {
     Color bgColor = Colors.transparent;
     Color borderColor = baseBorderColor(context).withOpacity(.5);
     final isRound = !intrinsic || exotic;
+    final isEnhanced = plugDef?.inventory?.tierType == TierType.Common;
     if (equipped && !intrinsic) {
       bgColor = perkColor(context).withOpacity(.5);
     }
@@ -80,6 +81,21 @@ class PerkIconWidget extends StatelessWidget {
                       border: Border.all(color: borderColor, width: 1.5 * scale),
                     ),
                   ),
+                  if (isEnhanced)
+                    Positioned.fill(
+                        child: Container(
+                      margin: EdgeInsets.all(4 * scale),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(_maxPerkIconSize),
+                          gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                              context.theme.achievementLayers.layer0,
+                              context.theme.achievementLayers.layer0.withOpacity(0),
+                            ],
+                          )),
+                    )),
                   Padding(
                       padding: EdgeInsets.all(intrinsic ? 0 : 4 * scale),
                       child: ManifestImageWidget<DestinyInventoryItemDefinition>(
