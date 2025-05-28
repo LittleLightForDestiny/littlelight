6a7111e716a101a331e4d1aac0f541d6625a941c
jaoryuken <jao@markezine.me>
2025-05-28 10:19:35 -0300

    fix workflows

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,7 +14,7 @@ import in_app_review
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite_darwin
+import sqflite
 import url_launcher_macos
 import wakelock_plus
 
