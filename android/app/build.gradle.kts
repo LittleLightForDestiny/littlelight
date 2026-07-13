40c9d17289443e0bf123df13275dbea1157e7237
João Paulo Marquesini <joao.marquesini@estrategia.com>
2026-07-13 13:28:28 -0300

    android project fixes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.android.application")
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 val localProperties = Properties()
