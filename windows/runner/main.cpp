952d042bb04d5872c3d7c7da3c4d625fe58c5774
João Paulo Marquesini <joao.marquesini@estrategia.com>
2026-07-18 19:44:50 -0300

    trying to fix auth

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -5,17 +5,9 @@
 #include "flutter_window.h"
 #include "utils.h"
 
-#include <desktop_webview_window/desktop_webview_window_plugin.h>
-
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command)
 {
-  // Captura e gerencia a janela de autenticação
-  if (desktop_webview_window::ProcessWindowLoopWithArgs())
-  {
-    return 0;
-  }
-
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent())
