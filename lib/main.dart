bcfd0b0394ea0f5b82232350b5f20e14b440d77e
João Paulo Marquesini <joao.marquesini@estrategia.com>
2026-07-18 19:51:14 -0300

    trying to fix auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:desktop_webview_window/desktop_webview_window.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
@@ -6,8 +7,11 @@ import 'package:little_light/services/setup.dart';
 
 import 'core/littlelight.app.dart';
 
-void main() async {
+void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (runWebViewTitleBarWidget(args)) {
+    return;
+  }
   await setupCoreServices();
   ExceptionHandler();
   runApp(Phoenix(child: const LittleLightApp()));
